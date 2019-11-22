456f6a4110a252645f9652bfb6bba16639f653ec
shokoe <shokoe@gmail.com>
2019-11-22 14:01:04 +0200

    Update a2top.sh

--- a2top.sh
+++ a2top.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) <year> <copyright holders>
+# Copyright (c) 2008 TehcDrift Ltd.
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
